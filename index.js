eca7607866bce091366a9b18b37c5511e2e490b4
Sama Loghmani <samahehloghmani@gmail.com>
2021-06-26 22:50:55 -0400

    create function for events

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ function calculate() {
     displayBox.innerText = eval(result)
 }
  document.querySelector(".calculate").addEventListener("click", calculate)
+
+ document.querySelector(".all-clear").addEventListener("click", function() {
+     return displayBox.innerText = 0;  
+ })
+
+document.querySelector(".last-clear").addEventListener("click", function() {
+    let text = displayBox.innerText;
+    if(text.length === 1) {
+        displayBox.innerText = 0;
+    } else {
+        displayBox.innerText = text.substring(0, text.length -1)
+    }
+})
